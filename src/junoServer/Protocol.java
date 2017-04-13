eb1240eaaa7514a6e86050ddb4e03cf4a50479f6
Ethan Brown <ethanbrown3@mail.weber.edu>
2017-04-13 15:43:38 -0600

    protocol.java • removed old login code • added login() method to handle receiving input from the user for their username. It will loop until there is an acknowledge message from the server.

--- src/junoServer/Protocol.java
+++ src/junoServer/Protocol.java
@@ -7,17 +7,21 @@ import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
+import javax.swing.JOptionPane;
+
 import org.json.JSONObject;
 
 public class Protocol {
 	private Receivable client;
 	private PrintWriter output;
-
-	public Protocol(String username, Receivable client) throws IOException {
+	private String username;
+	private BufferedReader input;
+	
+	public Protocol(Receivable client) throws IOException {
 		this.client = client;
 		Socket socket = new Socket();
 		String address = "ec2-52-41-213-54.us-west-2.compute.amazonaws.com";
-		BufferedReader input;
+		
 
 		int port = 8989;
 		try {
@@ -29,15 +33,32 @@ public class Protocol {
 
 		input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		output = new PrintWriter(socket.getOutputStream());
-
-		System.out.println("connected");
-		output.println("{\"type\":\"login\",\"message\":{\"username\":\"" + username + "\"}}");
-		output.flush();
-		System.out.println(input.readLine());
+		login();
+		
 
 	}
-
-	private void sendMessage(JSONObject message) {
+	private void login() {
+		boolean ack = false;
+		JSONObject message;
+		while (!ack) {
+			username = JOptionPane.showInputDialog("Enter a Username");
+			output.println("{\"type\":\"login\",\"message\":{\"username\":\"" + username + "\"}}");
+			output.flush();
+			try {
+				message = new JSONObject(input.readLine());
+				if (message.getString("type").equals("acknowledge")) {
+					ack = true;
+					System.out.println("connected");
+				} else {
+					client.giveMessage(message);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void sendMessage(JSONObject message) {
 		output.println(message);
 		output.flush();
 	}
